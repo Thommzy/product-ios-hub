66c070e9dc7f26d060ebe966233b550c53a1b0ba
Thommzy <tomiwaobeisun@yahoo.com>
2026-06-16 15:34:56 +0100

    reolve error and import Foundation

--- ios-product-hub/ios-product-hub/Data/Local/FavoritesLocalDataSource.swift
+++ ios-product-hub/ios-product-hub/Data/Local/FavoritesLocalDataSource.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftData
+import Foundation
 
 protocol FavoritesLocalDataSource {
     func fetchAll() -> [Int]
--- ios-product-hub/ios-product-hub/Data/Local/ProductLocalDataSource.swift
+++ ios-product-hub/ios-product-hub/Data/Local/ProductLocalDataSource.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftData
+import Foundation
 
 protocol ProductLocalDataSource {
     func fetchAll() -> [Product]
